edbad58da004381552bb8cf4a3ed544a9af44497
Niknemfay <niknemfover@gmail.com>
2024-02-06 17:03:01 +0200

    api

--- src/api/axios.api.ts
+++ src/api/axios.api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import { getTokenFromLocalStorage } from '../helpers/localstorage.helper'
 export const instance = axios.create({
-	baseURL: 'http://localhost:3000/api',
+	baseURL: 'https://server1-idmh.onrender.com/api',
 	headers: {
 		Authorization: 'Bearer ' + getTokenFromLocalStorage() || '',
 	},
